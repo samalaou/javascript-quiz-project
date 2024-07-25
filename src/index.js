4352c37a7fa07056fa427b688149e64d0653de55
Sam <sam.mjalaoui@gmail.com>
2024-07-25 17:08:45 +0200

    day 4

--- src/index.js
+++ src/index.js
@@ -45,23 +45,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   /************  SHOW INITIAL CONTENT  ************/
+  const timeRemainingSpan = document.querySelector("#timeRemaining span");
 
-  // Convert the time remaining in seconds to minutes and seconds, and pad the numbers with zeros if needed
-  const minutes = Math.floor(quiz.timeRemaining / 60).toString().padStart(2, "0");
-  const seconds = (quiz.timeRemaining % 60).toString().padStart(2, "0");
-
-  // Display the time remaining in the time remaining container
-  const timeRemainingContainer = document.getElementById("timeRemaining");
-  timeRemainingContainer.innerText = `${minutes}:${seconds}`;
-
-  // Show first question
+  // Initial display of time remaining
+  const updateDisplayTime = () => {
+    const minutes = Math.floor(quiz.timeRemaining / 60).toString().padStart(2, "0");
+    const seconds = (quiz.timeRemaining % 60).toString().padStart(2, "0");
+    timeRemainingSpan.innerText = `${minutes}:${seconds}`;
+  };
+  
+  // Display the initial time
+  updateDisplayTime();
+  
+  // Show the first question
   showQuestion();
-
-
+  
   /************  TIMER  ************/
-
+  
   let timer;
 
+  function startTimer(){
+    timer = setInterval(() => {
+      quiz.timeRemaining--;
+      updateDisplayTime();
+      if (quiz.timeRemaining <= 0) {
+        clearInterval(timer);
+        showResults()
+      }
+    }, 1000);
+  }
+
+  startTimer()
 
   /************  EVENT LISTENERS  ************/
 
@@ -181,9 +195,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   function showResults() {
+    // stop the timer before the end view and the quiz result are shown
+    clearInterval(timer);
 
-    // YOUR CODE HERE:
-    //
     // 1. Hide the quiz view (div#quizView)
     quizView.style.display = "none";
 
@@ -201,6 +215,9 @@ document.addEventListener("DOMContentLoaded", () => {
     quiz.correctAnswers = 0;
     quiz.shuffleQuestions();
     showQuestion()
+    quiz.timeRemaining = quizDuration;
+    updateDisplayTime();
+    startTimer();
   }
   
 });
